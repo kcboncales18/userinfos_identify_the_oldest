a2f7ad3e9d4da3388f3a8f3435de01e80a4693c7
kcboncales.18 <kirstencaryl.l.boncales@gmail.com>
2024-10-24 16:28:55 +0800

    the program will continue to ask

--- user_infos_identify_the_oldest.py
+++ user_infos_identify_the_oldest.py
@@ -8,3 +8,12 @@ def user_name (): #define the variables
 def user_age (): 
     age = int(input("Kindly, insert your age: "))
     return age 
+
+while True: 
+    name = user_name()
+    age = user_age ()
+
+    user_infos.append({"user_name":name, "user_age":age}) #format
+
+    print("User's informations")
+    print(user_infos) #the program continues to ask without stopping. 
