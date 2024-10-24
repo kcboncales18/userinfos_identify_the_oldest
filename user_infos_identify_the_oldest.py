78ff5203339bd0f6490dc8946a7b239bd5ab9a1c
kcboncales.18 <kirstencaryl.l.boncales@gmail.com>
2024-10-24 16:22:08 +0800

    defining the variables

---
+++ user_infos_identify_the_oldest.py
@@ -0,0 +1,10 @@
+#array
+user_infos = []
+
+def user_name (): #define the variables
+    name = input("Kindly, insert your name: ")
+    name.isalpha
+    return name
+def user_age (): 
+    age = int(input("Kindly, insert your age: "))
+    return age 
