f7aadacaaf535618d87d24fada063a435b5f0b4f
kcboncales.18 <kirstencaryl.l.boncales@gmail.com>
2024-10-24 16:47:02 +0800

    new variable for the oldest

--- user_infos_identify_the_oldest.py
+++ user_infos_identify_the_oldest.py
@@ -19,6 +19,13 @@ while True:
     print(user_infos) #the program continues to ask without stopping. 
 
 #asking the user to continue inserting another infos or not
-    retry = input("Do you want to insert another infos? Yes or no.")
+    retry = input("Do you want to insert another infos? Yes or no.") #the program will continue if yes, and stop if no. 
     if retry =="no":
         break
+#identifying the oldest
+def oldest(user_infos): #new variable
+    oldest_user = user_infos[0]
+    for another_user in user_infos: 
+        if another_user["user_age"] > oldest_user["user_age"]:
+            oldest_user = another_user
+    return oldest_user
